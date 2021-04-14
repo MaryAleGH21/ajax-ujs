b2214f956b12b73929a1778343e4fa098b3d575c
Alejandra <marialesr.17@gmail.com>
2021-04-14 12:27:57 -0600

    created model todo with scaffold

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,22 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// Llamada de ajax a través de jQuery
+
+// $.ajax({  // El ajax viene siendo como un fetch!
+// 	url: '/todos',
+// 	type: 'POST',
+// 	dataType: 'json',
+//   headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+//   data: {"todo": {"content": "Este es un todo creado desde ajax/js"}} //Crea un nuevo todo
+// })
+// .done(function(data) { //Done es lo mismo que then!
+// 	console.log(data);
+// })
+// .fail(function() { //Fail es como el método catch!
+// 	console.log("error");
+// })
+// .always(function() { //
+// 	console.log("complete");
+// });
